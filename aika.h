c8d1575db25c1479db3a6350a5a84cddf51f5185
Jani-siv <info@kengityspaja.fi>
2020-11-11 13:56:37 +0200

    updating files

--- aika.h
+++ aika.h
@@ -1,47 +1,16 @@
-//library for time functions for arduino
-// c Jani Sivonen
-//setting time for game
-void pelinTiedot*::alustaAika(int maara);
-//decrease time if player make error
-int pelinTiedot::vahennaAika(int maara);
-//update time in lcd screen
-void pelinTiedot::paivitaAika(int maara);
-// check how much time is left in game
-int pelinTiedot::tarkistaAika();
-
-void pelinTiedot*::alustaAika(int maara)			//time set in seconds
+#ifndef AIKA_H
+#define AIKA_H
+class pelinTiedot
 {
-	peliAika = maara;
-}
-
-int pelinTiedot::vahennaAika(int maara)
-{
-	if (maara > peliAika)
-	{
-		peliAika = peliAika - maara;
-		return 1;			//return succesfull 
-	}
-	else
-	{
-		gameOver = 1;
-		peliAika = 0;
-		return 0;			//time out
-	}
-}
-
-int pelinTiedot::tarkistaAika()
-{
-		return peliAika;
-}
-
-void pelinTiedot::paivitaAika(int maara)
-{
-	int aika = tarkistaAika();
-	aika = aika - maara;
-	peliAika = aika;
-}
-
-
-
-
+        private:
+                int peliAika;
+                int lukot;
+                int gameOver = 0;
+        public:
+                void alustaAika(int maara);
+                int vahennaAika(int maara); 
+                void paivitaAika(int maara);
+                int tarkistaAika();
+};
+#endif
 
--- main.cpp
+++ main.cpp
@@ -1,56 +1,12 @@
 //library for time functions for arduino
 // c Jani Sivonen
 //setting time for game
-class pelinTiedot
-{
-	private:
-		int peliAika;
-		int lukot;
-		int gameOver = 0;
-	public:
-		void alustaAika(int maara);
-		int vahennaAika(int maara);
-		void paivitaAika(int maara);
-		int tarkistaAika();
-};
+#include "aika.h"
 
 int main()
 {
+	new pelinTiedot peli;
 	return 0;
-}
-
-
-
-void pelinTiedot::alustaAika(int maara)                        //time set in seconds
-{
-	peliAika= maara;
-}
-
-int pelinTiedot::vahennaAika(int maara)
-{
-        if (maara > peliAika)
-        {
-                peliAika = peliAika - maara;
-                return 1;                       //return succesfull 
-        }
-        else
-        {
-                gameOver = 1;
-                peliAika = 0;
-                return 0;                       //time out
-        }
-}
-
-int pelinTiedot::tarkistaAika()
-{
-                return peliAika;
-}
-
-void pelinTiedot::paivitaAika(int maara)
-{
-        int aika = peliAika;
-        aika = aika - maara;
-	peliAika = aika;
 }
 
 
