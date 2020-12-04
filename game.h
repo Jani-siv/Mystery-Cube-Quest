3230ab6cd6756644da2700ce9da196398be20bb0
Jani-siv <info@kengityspaja.fi>
2020-12-04 16:24:18 +0200

    added game2

--- game.h
+++ game.h
@@ -5,12 +5,26 @@
 class game
 {
         public:
-                int init = 0;                                   //is game initialized 1 = yes
+                //global settings
+		//LED pinnien asetus
+		const int ledOut1 = 30;				//Game 2
+		const int ledOut2 = 32;
+		const int ledOut3 = 34;
+		const int ledOut4 = 36;
+		//BUTTON pinnien asetus
+		const int nappi1 = 31;
+		const int nappi2 = 33;
+		const int nappi3 = 35;
+		const int nappi4 = 37;	
+
+		int init = 0;                                   //is game initialized 1 = yes
                 int gameOver = 0;                               //game is over if 1
                 int startGame = 0;                              //push button to start game
                 int locks = 3;                                  //Amount of locks
                 int gameButton = 10;
-                int gameNumber[6] = {0x47,0x41,0x4D,0x45,0x20,0x30};    //game 
+               
+
+		int gameNumber[6] = {0x47,0x41,0x4D,0x45,0x20,0x30};    //game 
                 LCD lcdObjekti;                                 //instance of LCD class 
                 LCD* LCDOlio = &lcdObjekti;                     //pointer for lcdObjekti
                 aika aikaObjekti;                               //instance of aika class
@@ -27,11 +41,43 @@ class game
                 void winner();                                   //winning game
                 void setNumberInScreen(int num);                //Jonin game desimal in screen
                 void printGameNumber(int num);            //print game number in screen
-                void game1();                                   //jonin game                                                                
-                int roundNum = 3;                               //round number for games
+                
+		void game1();                                   //jonin game
+		int roundNum = 3;                               //round number for games
                 int randNum = 15;
                 int playNumber = 1;
-                void game2();                                   //Alexander game                                                            
+                
+		void game2();                                   //Alexander game 
+		//led tarkistus
+		int led1 = 0;					//led check
+		int led2 = 0;
+		int led3 = 0;
+		int led4 = 0;
+		//BUTTON stop painalus
+		int nappi1Stop = 1;				//button check
+		int nappi2Stop = 1;
+		int nappi3Stop = 1;
+		int nappi4Stop = 1;
+		//Random number
+		int randomArvo;
+		int timerRANDOM;
+		int timerUPDATE;
+		//Taulukko ja muuttujat
+		int randomTable[7];
+		int buttonTable[7];
+		int randomVariable  = 1;
+		int buttonVariable  = 1;
+		//Game difficulty
+		int vaikeus = 0;
+		int vaikeusAika = 0;  //<-- if (vaikeus == 0) {vaikeusAika = 500}, if (vaikeus == 1) {vaikeusAika = 400} etc.
+		int vaikeusTarkistus = 0;
+		int game1Rounds = 0;
+		bool timerON       = false; // <-- if true- timer doesn't work, false- start timer
+		bool timerUpdateON = false; // <-- In truth that don't work how it need to be, but it good working for timerON update :/
+		//Keskeytyksen muuttujat
+		volatile unsigned long int timerMillis1RANDOM       = 0;
+		volatile unsigned long int timerMillis1UPDATE       = 0; 
+
                 void game3();                                   //Jani game       
 		// 3. game controller
 		int rotate();					//return value of direction
